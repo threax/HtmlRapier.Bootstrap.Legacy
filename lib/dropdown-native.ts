b125bfa406c6757ac5298eafaca0e0f41818336c
Andrew Piper <piper.andrew@spcollege.edu>
2017-06-07 14:31:02 -0400

    Added a toggle for bootstrap drop downs.

--- lib/dropdown-native.ts
+++ lib/dropdown-native.ts
@@ -1,50 +1,97 @@
 // Native Javascript for Bootstrap 3 | Dropdown
 // by dnp_theme
 
+import { ActionEventDispatcher } from 'hr.eventdispatcher';
+import * as toggles from 'hr.toggles';
+
 // DROPDOWN DEFINITION
 // ===================
-export function Dropdown( element) {
-  this.menu = typeof element === 'object' ? element : document.querySelector(element);
-  var self = this;
+export function Dropdown(element) {
+    this.menu = typeof element === 'object' ? element : document.querySelector(element);
+    var self = this;
 
-  this.handle = function(e) { // fix some Safari bug with <button>
-    var target = e.target || e.currentTarget,
-        children = [], c = self.menu.parentNode.getElementsByTagName('*');
-    /\#$/g.test(target.href) && e.preventDefault();
+    this.handle = function (e) { // fix some Safari bug with <button>
+        var target = e.target || e.currentTarget,
+            children = [], c = self.menu.parentNode.getElementsByTagName('*');
+        /\#$/g.test(target.href) && e.preventDefault();
 
-    for ( var i=0, l = c.length||0; i<l; i++) { l && children.push(c[i]); }
-    if ( target === self.menu || target.parentNode === self.menu || target.parentNode.parentNode === self.menu ) {
-      self.toggle(e);
-    }  else if ( children && children.indexOf(target) > -1  ) {
-      return;
-    } else { self.close(); }
-  }
-  this.toggle = function(e) {
-    if (/\bopen/.test(this.menu.parentNode.className)) {
-      this.close();
-      document.removeEventListener('keydown', this.key, false);
-    } else {
-      this.menu.parentNode.className += ' open';
-      this.menu.setAttribute('aria-expanded',true);
-      document.addEventListener('keydown', this.key, false);
+        for (var i = 0, l = c.length || 0; i < l; i++) { l && children.push(c[i]); }
+        if (target === self.menu || target.parentNode === self.menu || target.parentNode.parentNode === self.menu) {
+            self.toggle(e);
+        } else if (children && children.indexOf(target) > -1) {
+            return;
+        } else { self.close(); }
     }
-  }
-  this.key = function(e) {
-    if (e.which == 27) {self.close();}
-  }
-  this.close = function() {
-    self.menu.parentNode.className = self.menu.parentNode.className.replace(/\bopen/,'');
-    self.menu.setAttribute('aria-expanded',false);
-  }
-  this.menu.setAttribute('tabindex', '0'); // Fix onblur on Chrome | Safari
-  document.addEventListener('click', this.handle, false);
+    this.toggle = function (e) {
+        if (/\bopen/.test(this.menu.parentNode.className)) {
+            this.close();
+            document.removeEventListener('keydown', this.key, false);
+        } else {
+            this.menu.parentNode.className += ' open';
+            this.menu.setAttribute('aria-expanded', true);
+            document.addEventListener('keydown', this.key, false);
+        }
+    }
+    this.key = function (e) {
+        if (e.which == 27) { self.close(); }
+    }
+    this.close = function () {
+        self.menu.parentNode.className = self.menu.parentNode.className.replace(/\bopen/, '');
+        self.menu.setAttribute('aria-expanded', false);
+    }
+    this.menu.setAttribute('tabindex', '0'); // Fix onblur on Chrome | Safari
+    document.addEventListener('click', this.handle, false);
 };
+
+//Toggle Plugin
+class DropdownStates extends toggles.ToggleStates {
+    private drop;
+
+    constructor(element, next) {
+        super(next);
+        this.drop = new Dropdown(element);
+        this.addState('on', 'on');
+        this.addState('off', 'off');
+    }
+
+    public get onEvent() {
+        return this.drop.openEvent;
+    }
+
+    public get offEvent() {
+        return this.drop.closeEvent;
+    }
+
+    public activateState(state) {
+        switch (state) {
+            case 'on':
+                if (!this.drop.isOpen()) {
+                    this.drop.open();
+                }
+                break;
+            case 'off':
+                if (this.drop.isOpen()) {
+                    this.drop.close();
+                }
+                break;
+        }
+    }
+}
 
 // DROPDOWN DATA API
 // =================
-export function activate(){
-  var Dropdowns = document.querySelectorAll('[data-toggle=dropdown]'), i = 0, ddl = Dropdowns.length;
-  for (i; i < ddl; i++) {
-      new Dropdown(Dropdowns[i]);
-  }
+export function activate() {
+    var Dropdowns = document.querySelectorAll('[data-toggle=dropdown]'), i = 0, ddl = Dropdowns.length;
+    for (i; i < ddl; i++) {
+        new Dropdown(Dropdowns[i]);
+    }
+
+    //Html Rapier Plugin
+    toggles.addTogglePlugin(function (element, states, toggle) {
+        if (element.classList.contains('dropdown-toggle')) {
+            toggle = new DropdownStates(element, toggle);
+        }
+
+        return toggle;
+    });
 }
