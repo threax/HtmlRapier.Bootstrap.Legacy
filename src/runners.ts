a5ad6f046bffe0ff53ecda854db232819c9d81bb
Andrew Piper <piper.andrew@spcollege.edu>
2018-11-15 13:45:22 -0500

    Removed bootstrap4form, this is in another library now.

--- src/runners.ts
+++ src/runners.ts
@@ -1,16 +1,3 @@
 declare var jsns;
-declare var $;
-
-var bootstrapVersion = $.fn.collapse.Constructor.VERSION;
-if (bootstrapVersion === undefined) {
-    throw new Error("Cannot detect bootstrap version. Bootstrap plugin will not be available.");
-}
-var split = bootstrapVersion.split(".");
-var version = split[0];
-switch(version){
-    case '4':
-        jsns.run("hr.bootstrap.bootstrap4form");
-        break;
-}
 
 jsns.run("hr.bootstrap.activate");
