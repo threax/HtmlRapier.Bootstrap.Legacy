3651a697bb80ef2323e5d0617d5ff2d125e5b90c
Andrew Piper <andrew@threax.com>
2020-07-01 13:58:51 -0400

    Removed runners, must now call setup function.

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import * as modal from 'hr.bootstrap.modal';
+import * as dropdown from 'hr.bootstrap.dropdown';
+import * as tab from 'hr.bootstrap.tab';
+
+modal.activate();
+dropdown.activate();
+tab.activate();
+
+export function setup() {
+    //Does not do anything, but makes module work
+    return true;
+}
